d094520d31c86ebeb662f73cbfec1d354a6f6b45
Keyboard-Genius <56653927+Keyboard-Genius@users.noreply.github.com>
2021-12-02 06:31:02 -0500

    Updated incorrect filenames

--- Day2/problem4.py
+++ Day2/problem4.py
@@ -1,18 +1,20 @@
 def main():
     depth = 0
     hPos = 0
+    aim = 0
     with open('./input2.txt', 'r') as input:
         data = input.readlines()
         for line in data:
             command = line.split(' ')
             if command[0] == 'forward':
                 hPos += int(command[1])
+                depth += aim * int(command[1])
             elif command[0] == 'up':
-                depth -= int(command[1])
+                aim -= int(command[1])
             elif command[0] == 'down':
-                depth += int(command[1])
+                aim += int(command[1])
         
-    print(f'Depth: {depth}, Horizontal position: {hPos}, Multiplied: {hPos * depth}')
+    print(f'Depth: {depth}, Horizontal position: {hPos}, Aim: {aim}, Multiplied: {hPos * depth}')
 
 if __name__ == "__main__":
     main()
